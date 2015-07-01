a02ac8832a27a7216a79b576a0218b12fd222b81
vamshi <vamshi.kolanu@gmail.com>
2015-07-01 14:57:45 +0530

    play andpuase has been done

--- js/background.js
+++ js/background.js
@@ -1,19 +1,91 @@
-chrome.runtime.onMessage.addListener(function(message, sender) {
-
-	 if(message == "show_popup"){
-	 		
-	 		console.log("Init Called");
-            var ROOT = 'https://the-tasty-samosa.appspot.com/_ah/api';
-            gapi.client.load('samosa', 'v1', function() {
-                console.log("Api loaded");
-            }, ROOT);
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+    
+    if(request.msg == "load_popup") {
         
-        gapi.client.samosa.api.expressions.popular().execute(
-            function(resp) {
-                console.log(resp);
-        });
-	 }
-	 else{
-   	 	chrome.tabs.sendMessage(sender.tab.id, message);
-   	}
+        
+        get_voices();
+
+        $.get(chrome.extension.getURL("../modal.html"), function(response){
+            
+            sendResponse({html: response});
+
+        },'html');
+        
+    }
+
+
 });
+
+chrome.runtime.onMessage.addListener(function(message) {
+    if (message && message.type == 'copy') {
+        var input = document.createElement('textarea');
+        document.body.appendChild(input);
+        input.value = message.text;
+        input.focus();
+        input.select();
+        document.execCommand('Copy');
+        input.remove();
+    }
+});
+
+
+get_voices = function() {
+        var ROOT = 'https://the-tasty-samosa.appspot.com/_ah/api';
+        return gapi.client.load('samosa', 'v1', function() {
+          popular_now();
+        }, ROOT);
+
+
+}
+
+popular_now = function() {
+
+    var popular_voices = gapi.client.samosa.api.expressions.popular().execute(
+      function(resp) {
+
+
+             chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+              chrome.tabs.sendMessage(tabs[0].id, {voices: resp.voices});
+            });
+    });
+
+ }
+
+// chrome.runtime.onMessage.addListener(function(message, sender) {
+
+//   if(message.msg == "load_popup") {
+
+//     $.get(chrome.extension.getURL("../modal.html"),function(response){
+
+
+//     },'html');
+
+//       $.ajax({
+//             url: chrome.extension.getURL("../modal.html"),
+//             dataType: "html",
+//             success: sendResponse
+//         });
+//   }
+
+//   if (message == "show_popup") {
+
+  
+//     var ROOT = 'https://the-tasty-samosa.appspot.com/_ah/api';
+//     gapi.client.load('samosa', 'v1', function() {
+//         popular_now();
+//     }, ROOT);
+
+//   }
+
+//   else {
+//     chrome.tabs.sendMessage(sender.tab.id, message);
+//   }
+
+// });
+
+// popular_now = function() {
+//   var popular_voices = gapi.client.samosa.api.expressions.popular().execute(
+//     function(resp) {
+//      //  chrome.runtime.sendMessage({'voices':resp.voices});
+//     });
+// });
--- js/main.js
+++ js/main.js
@@ -5,36 +5,59 @@
    }, 8000);
  });
 
- $(document).on("click", ".samosa_comment", function() {
-
-   if ($('#samosa-frame').length == 0) {
-     var iframe = document.createElement('iframe');
-     iframe.src = chrome.extension.getURL("../modal.html");
-     iframe.className = 'css-isolation-popup';
-     iframe.frameBorder = 0;
-     iframe.id = "samosa-frame";
-     document.body.appendChild(iframe);
-     chrome.runtime.sendMessage('show_popup');
-   }
-   else {
-     $('#samosa-frame').show();
-   }
-
- });
-
- chrome.runtime.onMessage.addListener(function(message) {
-   if (message == 'hide_popup') {
-     $('#samosa-frame').hide();
-   }
-   else {
-     console.log(message);
-   }
- });
-
  $(window).scroll(function() {
    comment_box_icon();
  });
 
+ $(document).on("click", ".samosa_comment", function(e) {
+
+;
+
+   var parent_span  = $(this).parent().parent().siblings().find('span');
+
+   var data_reactid = parent_span.children().attr('data-reactid');
+
+   parent_span.children().remove();
+   parent_span.append('<span data-reactid="'+data_reactid+'.0">asdasdasd</span>')
+
+   $(this).parent().parent().siblings().find('._5ywb').remove()
+
+
+   if ($('.samosa-modal').length == 0) {
+
+
+     chrome.extension.sendRequest({
+       msg: "load_popup"
+     }, function(response) {
+       $("body").append(response.html);
+     });
+   }
+   else {
+     $('.samosa-modal').show();
+   }
+
+ });
+
+
+ chrome.runtime.onMessage.addListener(
+   function(request, sender, sendResponse) {
+     add_voices(request.voices);
+   });
+
+
+
+ add_voices = function(voices) {
+   for (i = 0; i < voices.length; i++) {
+
+     $('.samosa-clips').append('<div class="samosa-audioclip"> \
+        <img width="120" height="120" src="' + voices[i].poster_url + '" style="position: relative; top: 0; left: 0;"/> \
+        <img class="player" id ="play" src="'+ chrome.extension.getURL("../play.png") +'" style="width:30px;position: absolute; top: 43px; left: 45px; cursor:pointer;"/> \
+        <span style="display:none;" class="audioclip-path">' + voices[i].mp3_url + '</span> \
+        <button class="samosa-btn" style="height:25px;width:120px;cursor:pointer;font-size:8px;">CLICK TO COPY</button> \
+      </div> \
+      ');
+   }
+ }
 
  comment_box_icon = function() {
    var img_url = chrome.extension.getURL('../icon.png');
@@ -46,3 +69,64 @@
    $(new_comment).append('<div class="comment_box_icon"><img class="samosa_comment" width="22px" src=' + img_url + '></div>');
 
  }
+
+$(document).on('click', '.samosa-btn', function(e){
+
+      var link = $(this).parent().find('span').text();
+
+      console.log($(this).parent().find('span').text());
+
+      chrome.runtime.sendMessage({
+         type: 'copy',
+         text: link
+      });
+
+})
+
+$(document).on('click','.samosa-overlay', function(e) {
+    $('.samosa-modal').hide();
+  });
+
+
+$('audio').on('ended', function() {
+  
+  console.log('hieee');
+
+});
+
+$(document).on('click', '.player', function (e) {
+
+       var id = $(this).attr('id');
+       if(id === "play") {
+  
+         var audioclip_path  = $(this).parent().find("span").text();
+         var myaudio = new Audio(audioclip_path);
+         myaudio.addEventListener("ended", function() { 
+              $($(this).siblings()[1]).attr('id', 'play');
+              $($(this).siblings()[1]).attr('src', chrome.extension.getURL("../play.png"));
+               
+          });
+
+         if(typeof $(this).siblings()[3] == "undefined") {
+          $(this).parent().append(myaudio);
+          $(this).siblings()[3].play();
+          $(this).attr('id','pause');
+          $(this).attr('src', chrome.extension.getURL("../pause.png"))
+         }
+        else{
+          $(this).siblings()[3].play();
+          $(this).attr('id','pause');
+          $(this).attr('src', chrome.extension.getURL("../pause.png"))
+        }
+       }
+       else{
+         $(this).siblings()[3].pause();
+         $(this).attr('id','play');
+         $(this).attr('src', chrome.extension.getURL("../play.png"))
+         }
+    });
+
+
+
+
+
